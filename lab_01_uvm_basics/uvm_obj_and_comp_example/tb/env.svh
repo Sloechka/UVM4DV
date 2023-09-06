74c20abba9ea1763f852c110c6dda3a39dc88ed0
Путря Федор Михайлович <fputrya@cooler2-pc.elvees.com>
2023-09-06 19:26:35 +0300

    remove 2 component declaration

--- lab_01_uvm_basics/uvm_obj_and_comp_example/tb/env.svh
+++ lab_01_uvm_basics/uvm_obj_and_comp_example/tb/env.svh
@@ -7,9 +7,8 @@ class env extends uvm_env;
     `uvm_component_utils(env)
     
     my_top_comp m_my_top_comp_0;
-    // insert component declaration here -->
-    my_top_comp m_my_top_comp_1;
-    my_top_comp m_my_top_comp_2;
+    // insert component declaration here --> (step 1)
+
    
     function new( string name , uvm_component parent );
         super.new( name , parent );
@@ -23,6 +22,6 @@ endclass
 function void env::build_phase(uvm_phase phase);
     super.build_phase(phase);
     m_my_top_comp_0 = my_top_comp::type_id::create("m_my_top_comp_0", this);
-    // insert component creation here --> (step1)
+    // insert component creation here --> (step 1)
 
 endfunction
